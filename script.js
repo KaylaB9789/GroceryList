a4b5bd5c9701b2cf084bf669aeaf14748edb4fe0
KaylaB9789 <kayla.brown9789@gmail.com>
2023-11-30 02:28:02 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,75 @@
+let groceryItems = [
+{ name: 'Apple', category: 'Fruits', price: 1.0, quantity: 6 },
+{ name: 'Banana', category: 'Fruits', price: 0.5, quantity: 10 },
+{ name: 'Carrot', category: 'Vegetables', price: 0.8, quantity: 8 },
+{ name: 'Milk', category: 'Dairy', price: 2.0, quantity: 2 },
+{ name: 'Eggs', category: 'Dairy', price: 1.5, quantity: 12 }];
+
+
+const groceryList = document.getElementById('groceryList');
+const totalCostElement = document.getElementById('totalCost');
+const totalQuantityElement = document.getElementById('totalQuantity');
+
+function displayGroceryItems() {
+  groceryList.innerHTML = '';
+  let totalCost = 0;
+  let totalQuantity = 0;
+
+  groceryItems.forEach((item) => {
+    const itemDiv = document.createElement('div');
+    itemDiv.innerHTML = `<b>${item.name}</b> (${item.category}): $${item.price.toFixed(2)} - ${item.quantity} units`;
+    groceryList.appendChild(itemDiv);
+
+    totalCost += item.price * item.quantity;
+    totalQuantity += item.quantity;
+  });
+
+  totalCostElement.textContent = totalCost.toFixed(2);
+  totalQuantityElement.textContent = totalQuantity;
+}
+
+function isolateCategory(category) {
+  groceryList.innerHTML = '';
+  const filteredItems = groceryItems.filter((item) => item.category === category);
+
+  filteredItems.forEach((item) => {
+    const itemDiv = document.createElement('div');
+    itemDiv.innerHTML = `<b>${item.name}</b> (${item.category}): $${item.price.toFixed(2)} - ${item.quantity} units`;
+    groceryList.appendChild(itemDiv);
+  });
+}
+
+function showAllCategories() {
+  displayGroceryItems();
+}
+
+function orderItemsByCost() {
+  groceryItems.sort((a, b) => b.price - a.price);
+  displayGroceryItems();
+}
+
+function addItemPrompt() {
+  const name = prompt('Enter the name of the item:');
+  const category = prompt('Enter the category of the item:');
+  const price = prompt('Enter the price of the item:');
+  const quantity = prompt('Enter the quantity of the item:');
+
+  if (!name || !category || !price || !quantity) {
+    alert('Please provide all information for the item.');
+    return;
+  }
+
+  const priceValue = parseFloat(price);
+  const quantityValue = parseInt(quantity);
+
+  if (isNaN(priceValue) || isNaN(quantityValue)) {
+    alert('Please enter valid numeric values for price and quantity.');
+    return;
+  }
+
+  const newItem = { name, category, price: priceValue, quantity: quantityValue };
+  groceryItems.push(newItem);
+  displayGroceryItems();
+}
+
+displayGroceryItems();
